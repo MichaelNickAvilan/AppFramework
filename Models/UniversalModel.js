af428e252d40823c4f5b9d68e6d18954323bffb5
Michael Nick Avilán Mora <michael.avilan@gmail.com>
2016-07-22 16:47:05 -0500

    Delete vars
    no necessary vars

--- Models/UniversalModel.js
+++ Models/UniversalModel.js
@@ -14,23 +14,11 @@
     a_users: [],
     a_products: [],
     a_services: [],
-    a_universities: [],
     a_countries_visitor: [],
     a_main_container:{},
     a_sended_object:{},
     
-    a_db_path:'http://hechoparaliderar.com/admin/com/php/proxys/',
-    a_landings_proxy:'landingsproxy.php',
-    a_social_proxy:'socialsproxy.php',
-    a_culture_proxy:'culturesproxy.php',
-    a_section_proxy:'sectionsproxy.php',
-    a_modules_proxy:'moduleproxy.php',
-    a_banners_proxy:'bannersproxy.php',
-    a_posts_proxy:'postsproxy.php',
-    a_formsLanding_proxy:'formsLandingproxy.php',
-    a_leads_proxy:'leadsproxy.php',
-    a_page_users_proxy:'pageusersproxy.php',
-    
+    a_db_path:'',
     init: function () {
     }
 };
